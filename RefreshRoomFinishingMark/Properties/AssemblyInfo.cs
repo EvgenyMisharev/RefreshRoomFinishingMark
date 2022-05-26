ddb31f4bb0c0d16252459e3f1c03b1cc0f2fb9c6
Евгений Мишарев <misharevea@gmail.com>
2022-05-26 15:23:11 +0300

    Добавьте файлы проекта.

---
+++ RefreshRoomFinishingMark/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("RefreshRoomFinishingMark")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RefreshRoomFinishingMark")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("885d340d-9b39-497d-b4f3-b004875e0fab")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RefreshRoomFinishingMark/RefreshRoomFinishingMarkCommand.cs
@@ -0,0 +1,148 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefreshRoomFinishingMark
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    class RefreshRoomFinishingMarkCommand : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+            Room roomFromParam = new FilteredElementCollector(doc)
+                .WhereElementIsNotElementType()
+                .OfClass(typeof(SpatialElement))
+                .Where(e => e.GetType() == typeof(Room))
+                .Cast<Room>()
+                .FirstOrDefault(r => r.Area > 0);
+
+            if (roomFromParam == null)
+            {
+                TaskDialog.Show("Revit", "В проекте нет размещенных помещений!");
+                return Result.Cancelled;
+            }
+
+            //АР_ТипПола_Плагин
+            Guid floorTypePluginParamGUID = new Guid("34eac6db-112c-49fc-a3dc-7e3470b9656f");
+            Parameter floorTypePluginParam = roomFromParam.get_Parameter(floorTypePluginParamGUID);
+            if(floorTypePluginParam == null)
+            {
+                TaskDialog.Show("Revit", "В проекте отсутствует параметр \"АР_ТипПола_Плагин\"!");
+                return Result.Cancelled;
+            }
+            //АР_ОтделкаСтен_Плагин
+            Guid wallFinishPluginParamGUID = new Guid("326001d8-4e61-494a-8eeb-74e190e11bcf");
+            Parameter wallFinishPluginParam = roomFromParam.get_Parameter(wallFinishPluginParamGUID);
+            if (wallFinishPluginParam == null)
+            {
+                TaskDialog.Show("Revit", "В проекте отсутствует параметр \"АР_ОтделкаСтен_Плагин\"!");
+                return Result.Cancelled;
+            }
+            //АР_ОтделкаСтенСнизу_Плагин
+            Guid bottomWallFinishPluginParamGUID = new Guid("ee4a5627-a73d-40fa-80a9-d8702abb2f89");
+            Parameter bottomWallFinishPluginParam = roomFromParam.get_Parameter(bottomWallFinishPluginParamGUID);
+            if (bottomWallFinishPluginParam == null)
+            {
+                TaskDialog.Show("Revit", "В проекте отсутствует параметр \"АР_ОтделкаСтенСнизу_Плагин\"!");
+                return Result.Cancelled;
+            }
+            //АР_ОтделкаПотолка_Плагин
+            Guid ceilingFinishPluginParamGUID = new Guid("08ea6382-373d-4347-b723-4eab2427d250");
+            Parameter ceilingFinishPluginParam = roomFromParam.get_Parameter(ceilingFinishPluginParamGUID);
+            if (ceilingFinishPluginParam == null)
+            {
+                TaskDialog.Show("Revit", "В проекте отсутствует параметр \"АР_ОтделкаПотолка_Плагин\"!");
+                return Result.Cancelled;
+            }
+
+            //АР_ТипПола_Ключ
+            Parameter floorTypeKeyParam = roomFromParam.LookupParameter("АР_ТипПола_Ключ");
+            if (floorTypeKeyParam == null)
+            {
+                TaskDialog.Show("Revit", "В проекте отсутствует параметр \"АР_ТипПола_Ключ\"!");
+                return Result.Cancelled;
+            }
+            //АР_ОтделкаСтен_Ключ
+            Parameter wallFinishKeyParam = roomFromParam.LookupParameter("АР_ОтделкаСтен_Ключ");
+            if (wallFinishKeyParam == null)
+            {
+                TaskDialog.Show("Revit", "В проекте отсутствует параметр \"АР_ОтделкаСтен_Ключ\"!");
+                return Result.Cancelled;
+            }
+            //АР_ОтделкаСтенСнизу_Ключ
+            Parameter bottomWallFinishKeyParam = roomFromParam.LookupParameter("АР_ОтделкаСтенСнизу_Ключ");
+            if (bottomWallFinishKeyParam == null)
+            {
+                TaskDialog.Show("Revit", "В проекте отсутствует параметр \"АР_ОтделкаСтенСнизу_Ключ\"!");
+                return Result.Cancelled;
+            }
+            //АР_ОтделкаПотолка_Ключ
+            Parameter ceilingFinishKeyParam = roomFromParam.LookupParameter("АР_ОтделкаПотолка_Ключ");
+            if (ceilingFinishKeyParam == null)
+            {
+                TaskDialog.Show("Revit", "В проекте отсутствует параметр \"АР_ОтделкаПотолка_Ключ\"!");
+                return Result.Cancelled;
+            }
+
+            List<Room> roomList = new FilteredElementCollector(doc)
+                .WhereElementIsNotElementType()
+                .OfClass(typeof(SpatialElement))
+                .Where(e => e.GetType() == typeof(Room))
+                .Cast<Room>()
+                .Where(r => r.Area > 0)
+                .ToList();
+            using(Transaction t = new Transaction(doc))
+            {
+                t.Start("Обновление марок помещений");
+                foreach (Room room in roomList)
+                {
+                    if (room.LookupParameter("АР_ТипПола_Ключ").AsElementId().IntegerValue == -1)
+                    {
+                        room.get_Parameter(floorTypePluginParamGUID).Set("");
+                    }
+                    else
+                    {
+                        room.get_Parameter(floorTypePluginParamGUID).Set(room.LookupParameter("АР_ТипПола_Ключ").AsValueString());
+                    }
+
+                    if(room.LookupParameter("АР_ОтделкаСтен_Ключ").AsElementId().IntegerValue == -1)
+                    {
+                        room.get_Parameter(wallFinishPluginParamGUID).Set("");
+                    }
+                    else
+                    {
+                        room.get_Parameter(wallFinishPluginParamGUID).Set(room.LookupParameter("АР_ОтделкаСтен_Ключ").AsValueString());
+                    }
+
+                    if(room.LookupParameter("АР_ОтделкаСтенСнизу_Ключ").AsElementId().IntegerValue == -1)
+                    {
+                        room.get_Parameter(bottomWallFinishPluginParamGUID).Set("");
+                    }
+                    else
+                    {
+                        room.get_Parameter(bottomWallFinishPluginParamGUID).Set(room.LookupParameter("АР_ОтделкаСтенСнизу_Ключ").AsValueString());
+                    }
+                    
+                    if(room.LookupParameter("АР_ОтделкаПотолка_Ключ").AsElementId().IntegerValue == -1)
+                    {
+                        room.get_Parameter(ceilingFinishPluginParamGUID).Set("");
+                    }
+                    else
+                    {
+                        room.get_Parameter(ceilingFinishPluginParamGUID).Set(room.LookupParameter("АР_ОтделкаПотолка_Ключ").AsValueString());
+                    }
+                }
+                t.Commit();
+            }
+
+            TaskDialog.Show("Revit","Обработка завершена!");
+            return Result.Succeeded;
+        }
+    }
+}
